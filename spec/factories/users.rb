1c7aadc81b6d6c2a0abbf8f4d929a93201facdac
Miguel Angel Ibarra, Jr <miggs.ibarra@gmail.com>
2017-11-17 15:10:28 -0800

    adds validations for email format

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,10 +2,10 @@ FactoryBot.define do
   factory :user do
     fake_first_name = Faker::Name.first_name
     fake_last_name = Faker::Name.last_name
-    
+
     first_name fake_first_name
     last_name fake_last_name
-    email "#{fake_first_name}.#{fake_last_name}@example.com"
+    email "#{fake_first_name.downcase}.#{fake_last_name.downcase}@example.com"
     password "Pa$$w0rd"
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,30 @@
 require 'rails_helper'
 
 describe User, type: :model do
-  let(:user_example) { build(:user) }
-
   describe "Validations" do
+
     it { is_expected.to validate_presence_of(:first_name) }
     it { is_expected.to validate_presence_of(:last_name) }
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_presence_of(:password_hash) }
     subject { FactoryBot.create(:user) }
     it { is_expected.to validate_uniqueness_of(:email) }
+
+    context "when the @ character is not included" do
+      it "is not valid without a properly formated email" do
+        user_example = FactoryBot.build(:user)
+        user_example.email = "hello"
+        expect(user_example).to_not be_valid
+      end
+    end
+
+    context "when no local-part is privided for the mailbox" do
+      it "is not valid without a properly formated email" do
+        user_example = FactoryBot.build(:user)
+        user_example.email = "@hello"
+        expect(user_example).to_not be_valid
+      end
+    end
+
   end
 end
