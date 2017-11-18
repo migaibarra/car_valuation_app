69b37698eb74bdbe7a47049ec5c93229aea2be9f
Miguel Angel Ibarra, Jr <miggs.ibarra@gmail.com>
2017-11-17 16:06:33 -0800

    Commences with controller testing

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 describe SessionsController, type: :controller do
-  it { is_expected.to route(:get, '/sessions/new').to(action: 'new') }
+  it { is_expected.to route(:get, '/sessions/new').to(action: 'new') } # FALSE POSITIVE
+  # it { is_expected.to render_template('login') } # NEED TO IMPLEMENT
 end
