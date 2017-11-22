d1ed680ac7137889d28e09508b1eba0d073398a5
Miguel Angel Ibarra, Jr <miggs.ibarra@gmail.com>
2017-11-22 11:58:31 -0800

    should route GET /sessions/new to/from {:action=>new, :controller=>sessions}

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe UsersController, type: :controller do
-
+describe UsersController, type: :controller do
+  context "when user navigates to the user login page" do
+    it { route(:get, '/sessions/new').to(action: new) }
+  end
 end
